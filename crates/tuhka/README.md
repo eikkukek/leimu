94fadbdf2752c8073aec021827446bd7b9751ffc
Eikku <makirantaeinari@gmail.com>
2026-04-24 16:14:24 +0300

    Update tuhka

--- crates/tuhka/README.md
+++ crates/tuhka/README.md
@@ -1,4 +1,7 @@
 # tuhka
-Vulkan bindings for [leimu].
+New up-to-date Vulkan bindings used by [leimu].
+
+Follows mostly the same structure as [ash], but differs in some areas.
 
 [leimu]: https://crates.io/crates/leimu
+[ash]: https://crates.io/crates/ash
--- crates/tuhka/src/lib.rs
+++ crates/tuhka/src/lib.rs
@@ -1,3 +1,32 @@
+//! New up-to-date Vulkan bindings.
+//!
+//! # Usage
+//! ```
+//! use tuhka::{vk, Library, Instance};
+//! let library = unsafe {
+//!      Library::load()
+//! }.unwrap();
+//! let app_name = c"Test";
+//! let engine_name = c"Engine";
+//! let application_info = vk::ApplicationInfo {
+//!     p_application_name: app_name.as_ptr(),
+//!     application_version: vk::make_api_version(0, 1, 0, 0),
+//!     p_engine_name: engine_name.as_ptr(),
+//!     engine_version: vk::make_api_version(0, 1, 0, 0),
+//!     api_version: vk::API_VERSION_1_4,
+//!     ..Default::default()
+//! };
+//! let create_info = vk::InstanceCreateInfo {
+//! p_application_info: &application_info,
+//!     ..Default::default()
+//! };
+//! let instance: Instance = unsafe {
+//!     library.create_instance(&create_info, None)
+//! }.unwrap().value;
+//! unsafe {
+//!     library.destroy_instance(&instance, None);
+//! }
+
 #![no_std]
 
 pub mod vk;
